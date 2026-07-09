6a182ed169daeab89016bf5ea36d3f999e603db5
fatinnura <fatinmesly@gmail.com>
2026-07-10 00:01:03 +0800

    login page

---
+++ OMC_G10_Final/LoginPage.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace OMC_G10_Final
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            label1 = new Label();
+            txtPassword = new Label();
+            txtEmail = new TextBox();
+            txtEmailPassword = new TextBox();
+            btnLogin = new Guna.UI2.WinForms.Guna2Button();
+            btnRegister = new Guna.UI2.WinForms.Guna2Button();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 15F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.Info;
+            label1.Location = new Point(374, 392);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 31);
+            label1.TabIndex = 1;
+            label1.Text = "E-MAIL :";
+            // 
+            // txtPassword
+            // 
+            txtPassword.AutoSize = true;
+            txtPassword.Font = new Font("Verdana", 15F, FontStyle.Bold);
+            txtPassword.ForeColor = SystemColors.Info;
+            txtPassword.Location = new Point(314, 444);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(205, 31);
+            txtPassword.TabIndex = 2;
+            txtPassword.Text = "PASSWORD : ";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(560, 392);
+            txtEmail.Multiline = true;
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(320, 41);
+            txtEmail.TabIndex = 3;
+            // 
+            // txtEmailPassword
+            // 
+            txtEmailPassword.Location = new Point(560, 444);
+            txtEmailPassword.Multiline = true;
+            txtEmailPassword.Name = "txtEmailPassword";
+            txtEmailPassword.Size = new Size(320, 41);
+            txtEmailPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.CustomizableEdges = customizableEdges1;
+            btnLogin.DisabledState.BorderColor = Color.DarkGray;
+            btnLogin.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnLogin.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnLogin.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnLogin.FillColor = Color.FromArgb(255, 192, 192);
+            btnLogin.Font = new Font("Verdana", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = SystemColors.ActiveCaptionText;
+            btnLogin.Location = new Point(362, 533);
+            btnLogin.Name = "btnLogin";
+            btnLogin.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnLogin.Size = new Size(225, 56);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "LOG IN";
+            // 
+            // btnRegister
+            // 
+            btnRegister.CustomizableEdges = customizableEdges3;
+            btnRegister.DisabledState.BorderColor = Color.DarkGray;
+            btnRegister.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnRegister.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnRegister.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnRegister.FillColor = Color.FromArgb(255, 192, 192);
+            btnRegister.Font = new Font("Verdana", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRegister.ForeColor = Color.Black;
+            btnRegister.Location = new Point(614, 533);
+            btnRegister.Name = "btnRegister";
+            btnRegister.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnRegister.Size = new Size(225, 56);
+            btnRegister.TabIndex = 6;
+            btnRegister.Text = "REGISTER";
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.CustomizableEdges = customizableEdges5;
+            guna2PictureBox1.Image = (Image)resources.GetObject("guna2PictureBox1.Image");
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(410, 63);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2PictureBox1.Size = new Size(415, 303);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox1.TabIndex = 7;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(108, 117, 82);
+            ClientSize = new Size(1200, 725);
+            Controls.Add(guna2PictureBox1);
+            Controls.Add(btnRegister);
+            Controls.Add(btnLogin);
+            Controls.Add(txtEmailPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(txtPassword);
+            Controls.Add(label1);
+            Name = "LoginPage";
+            Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label txtPassword;
+        private TextBox txtEmail;
+        private TextBox txtEmailPassword;
+        private Guna.UI2.WinForms.Guna2Button btnLogin;
+        private Guna.UI2.WinForms.Guna2Button btnRegister;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+    }
+}
