8eb79601daa44bb7d9ba9dbf646aadbdb8f72951
imranaldelina <imranaldelina@icloud.com>
2026-07-11 23:12:58 +0800

    man

--- OMC_G10_Final/MainPage.cs
+++ OMC_G10_Final/MainPage.cs
@@ -14,12 +14,38 @@ namespace OMC_G10_Final
 
         private void btnMedicine_Click(object sender, EventArgs e)
         {
+            Form? currentForm = this.FindForm();
 
+            if (currentForm is MedecinePage)
+            {
+                return;
+            }
+
+            MedecinePage newForm = new MedecinePage();
+            newForm.Show();
+
+            if (currentForm != null)
+            {
+                currentForm.Hide(); // Use Hide instead of Close so the application loop stays alive
+            }
         }
 
         private void btnDailyNeeds_Click(object sender, EventArgs e)
         {
+            Form? currentForm = this.FindForm();
 
+            if (currentForm is DailyNeedsPage)
+            {
+                return;
+            }
+
+            DailyNeedsPage newForm = new DailyNeedsPage();
+            newForm.Show();
+
+            if (currentForm != null)
+            {
+                currentForm.Hide(); // Use Hide instead of Close so the application loop stays alive
+            }
         }
 
         private void btnCall_Click(object sender, EventArgs e)
@@ -218,20 +244,7 @@ namespace OMC_G10_Final
 
         private void btnDailyNeeds_Click_1(object sender, EventArgs e)
         {
-            Form? currentForm = this.FindForm();
 
-            if (currentForm is DailyNeedsPage)
-            {
-                return;
-            }
-
-            DailyNeedsPage newForm = new DailyNeedsPage();
-            newForm.Show();
-
-            if (currentForm != null)
-            {
-                currentForm.Hide(); // Use Hide instead of Close so the application loop stays alive
-            }
         }
     }
 }
